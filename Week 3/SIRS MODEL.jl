5d1484d44c421436686a75e42590fcfc3bf57525
aajith99 <amarjith191104@gmail.com>
2024-10-04 18:32:06 +1000

    Adjusted paramters to include serevely infected

--- Week 3/SIRS MODEL.jl
+++ Week 3/SIRS MODEL.jl
@@ -1,32 +1,34 @@
 using DifferentialEquations
 using Plots
 
-function sir_model!(dN, N, p, t)
-    S, I, Is, R = N
-    c, B, ɣ, ps, ɣs, α = p
-
-    N_total = sum(N)  # Total population
+function sir_model!(du, u, p, t)
+    S, I, Is, R = u
+    c, β, ɣ, ps, ɣs, α = p
+    N = S + I + Is + R # Total population
     
-    dN[1] = - (c * B * I / N_total) * S + (α * R) # dS/dt
-    dN[2] = (c * B * I / N_total) * S - ɣ * I * (1 - ps) - (ɣ * ps * I) # dI/dt 
-    dN[3] = ɣ * I * (1 - ps) - (ɣs * Is) # dIs/dt
-    dN[4] = (ɣs * Is) + ɣ * I * (1 - ps) - (α * R) # dR/dt
+    du[1] = (-c * β * S * I / N) + (α * R) # dS/dt
+    du[2] = (c * β * S * I / N) - (ɣ * (1 - ps) * I) - (ɣ * ps * I) # dI/dt 
+    du[3] = (ɣ * ps * I) - (ɣs * Is) # dIs/dt
+    du[4] = (ɣs * Is) + (ɣ * (1 - ps) * I) - (α * R) # dR/dt
 end 
 
 # Parameters
-c = 10
-B = 0.035
-ɣ = 0.1
-α = 
+c = 10 
+β = 0.035
+ɣ = 0.1 # 10 days on average infections
+ps = 0.2 # 20% end up being seriously ill
+ɣs = 0.071 # Average 2 weeks to recover from serious illness
+α = 0.011 # 90 days or approximately 3 months to recover
 
-p = [c, B, ɣ, α]
+p = [c, β, ɣ, ps, ɣs, α]
 
 # Initial conditions
 S0 = 4999
 I0 = 1
+Is0 = 0
 R0 = 0
 
-u0 = [S0, I0, R0] 
+u0 = [S0, I0, Is0, R0] 
 
 # Time span
 tspan = (0.0,180.0)
@@ -38,7 +40,8 @@ prob = ODEProblem(sir_model!, u0, tspan, p)
 sol = solve(prob)
 
 # Plot the solution
-p1 = plot(sol, label=["Susceptible" "Infected" "Recovered"], xlabel="Time", ylabel="Population")
+p1 = plot(sol, label=["Susceptible" "Infected" "Severe Infected" "Recovered"], 
+xlabel="Time", ylabel="Population")
 
 
 #p1 = plot(sol.t,sol[2,:], xlimits=(0,23), ylimits=(0,150), 
